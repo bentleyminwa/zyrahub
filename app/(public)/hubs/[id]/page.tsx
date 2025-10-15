f713341eca09f86eefee2761e0f03ae0f6426ab5
bentleyminwa <bentleyminwa@gmail.com>
2025-10-15 05:34:20 +0300

    feat: create HubHero component to encapsulate hub display logic and improve code organization

--- app/(public)/hubs/[id]/page.tsx
+++ app/(public)/hubs/[id]/page.tsx
@@ -1,10 +1,9 @@
 'use client';
 
 import { assets } from '@/assets/assets';
-import { StarRating } from '@/components/ui/star-rating';
+import HubHero from '@/features/hubs/components/hub-hero';
 import type { Hub } from '@/features/hubs/types/validate';
-import { BadgeCheckIcon } from 'lucide-react';
-import Image from 'next/image';
+
 import { useParams } from 'next/navigation';
 
 export default function Hub() {
@@ -16,71 +15,7 @@ export default function Hub() {
 
   return (
     <main className='mt-5'>
-      <header className='relative'>
-        <Image
-          src={hub.photos[0].url}
-          alt={hub.photos[0].caption}
-          className='w-full h-[45vh] object-cover'
-        />
-
-        <div className='absolute inset-0 bg-linear-to-b from-black/0 via-black/50 to-black/80' />
-
-        {/* hub info */}
-        <section className='absolute bottom-0 left-60 mb-5 flex gap-5'>
-          {/* hub logo */}
-          <Image
-            src={hub.photos[0].url}
-            alt={hub.photos[0].caption}
-            className='w-[100px] h-[100px] rounded-full object-cover'
-          />
-
-          {/* hub content */}
-          <div className='space-y-3 text-white'>
-            <h2 className='font-bold text-5xl'>{hub.name}</h2>
-
-            <div className='flex items-center gap-2 text-lg'>
-              <StarRating rating={hub.rating} />
-              <span className=' font-semibold'>
-                {hub.rating}{' '}
-                <span className=''>({hub.review_count} Reviews)</span>
-              </span>
-            </div>
-
-            <div className='flex items-center gap-2'>
-              <p
-                className={`flex items-center gap-2 ${
-                  hub.is_claimed ? 'text-blue-500' : 'text-gray-500'
-                }`}
-              >
-                <BadgeCheckIcon />
-                {hub.is_claimed ? 'Claimed' : 'Not Claimed'}
-              </p>
-              |
-              {hub.highlights.map((highlight, index) => (
-                <p key={index}>{highlight}. </p>
-              ))}
-            </div>
-
-            <div className={`flex items-center gap-2`}>
-              <span
-                className={` ${
-                  hub.is_closed ? 'text-red-500' : 'text-green-500'
-                }`}
-              >
-                {hub.is_closed ? 'Closed' : 'Open'}
-              </span>
-
-              <span>
-                {hub.hours.map((hour, index) => (
-                  <p key={index}>
-                    {hour.open[0].start} - {hour.open[0].end}
-                  </p>
-                ))}
-              </span>
-            </div>
-          </div>
-        </section>
-      </header>
+      <HubHero hub={hub} />
     </main>
   );
 }
---
+++ features/hubs/components/hub-hero.tsx
@@ -0,0 +1,80 @@
+import { StarRating } from '@/components/ui/star-rating';
+import { BadgeCheckIcon } from 'lucide-react';
+import Image from 'next/image';
+import { Hub } from '../types/validate';
+
+interface HubHeroProps {
+  hub: Hub;
+}
+
+const HubHero = ({ hub }: HubHeroProps) => {
+  return (
+    <header className='relative'>
+      <Image
+        src={hub.photos[0].url}
+        alt={hub.photos[0].caption}
+        className='w-full h-[45vh] object-cover'
+      />
+
+      <div className='absolute inset-0 bg-linear-to-b from-black/0 via-black/50 to-black/80' />
+
+      {/* hub info */}
+      <section className='absolute bottom-0 left-60 mb-5 flex gap-5'>
+        {/* hub logo */}
+        <Image
+          src={hub.photos[0].url}
+          alt={hub.photos[0].caption}
+          className='w-[100px] h-[100px] rounded-full object-cover'
+        />
+
+        {/* hub content */}
+        <div className='space-y-3 text-white'>
+          <h2 className='font-bold text-5xl'>{hub.name}</h2>
+
+          <div className='flex items-center gap-2 text-lg'>
+            <StarRating rating={hub.rating} />
+            <span className=' font-semibold'>
+              {hub.rating}{' '}
+              <span className=''>({hub.review_count} Reviews)</span>
+            </span>
+          </div>
+
+          <div className='flex items-center gap-2'>
+            <p
+              className={`flex items-center gap-2 ${
+                hub.is_claimed ? 'text-blue-500' : 'text-gray-500'
+              }`}
+            >
+              <BadgeCheckIcon />
+              {hub.is_claimed ? 'Claimed' : 'Not Claimed'}
+            </p>
+            |
+            {hub.highlights.map((highlight, index) => (
+              <p key={index}>{highlight}. </p>
+            ))}
+          </div>
+
+          <div className={`flex items-center gap-2`}>
+            <span
+              className={` ${
+                hub.is_closed ? 'text-red-500' : 'text-green-500'
+              }`}
+            >
+              {hub.is_closed ? 'Closed' : 'Open'}
+            </span>
+
+            <span>
+              {hub.hours.map((hour, index) => (
+                <p key={index}>
+                  {hour.open[0].start} - {hour.open[0].end}
+                </p>
+              ))}
+            </span>
+          </div>
+        </div>
+      </section>
+    </header>
+  );
+};
+
+export default HubHero;
