39d23bd934448b49506fb763b04d98bdc6992363
bentleyminwa <bentleyminwa@gmail.com>
2025-10-21 07:02:01 +0300

    feat: add fallback loader to SignIn component for improved user experience

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -1,9 +1,16 @@
+import Loader from '@/components/shared/loader';
 import { SignIn } from '@clerk/nextjs';
 
 export default function SigninPage() {
   return (
     <div className='flex justify-center items-center h-screen'>
-      <SignIn />
+      <SignIn
+        fallback={
+          <div className='flex justify-center items-center h-screen'>
+            <Loader />
+          </div>
+        }
+      />
     </div>
   );
 }
---
+++ components/shared/loader.tsx
@@ -0,0 +1,7 @@
+import { LoaderCircle } from 'lucide-react';
+
+const Loader = () => {
+  return <LoaderCircle className='animate-spin' />;
+};
+
+export default Loader;
